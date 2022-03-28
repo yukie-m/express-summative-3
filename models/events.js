e26710c3d54636f10d2890cf3568a0c38c23c371
yukie-m <270141737@yoobeestudent.ac.nz>
2022-03-29 08:51:41 +1300

    add timestamps to mongo

--- models/events.js
+++ models/events.js
@@ -2,17 +2,20 @@ const mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var EventsSchema = new Schema({
-  eventname: String,
-  description: String,
-  host: String,
-  price: Number,
-  status: String,
-  thumb: String,
-  area: String,
-  category: String,
-  number: Number,
-});
+var EventsSchema = new Schema(
+  {
+    eventname: String,
+    description: String,
+    host: String,
+    price: Number,
+    status: String,
+    thumb: String,
+    area: String,
+    category: String,
+    number: Number,
+  },
+  { timestamps: true }
+);
 
 // singular capitalized name for the mongo collection - Products
 const Event = mongoose.model("Event", EventsSchema);
